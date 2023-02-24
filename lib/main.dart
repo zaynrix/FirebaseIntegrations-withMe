d2665037c294f34ba8ff7fde47004f01b07d4c92
mac <zayns1914@gmail.com>
2023-02-24 17:29:54 +0200

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -12,21 +13,74 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  FirebaseAuth auth = FirebaseAuth.instance;
+  var res;
+  bool isLogin = false;
+  @override
+  void initState() {
+    auth.authStateChanges().listen((event) {
+      if (auth.currentUser == null) {
+        setState(() {});
+        isLogin = false;
+      } else {
+        isLogin = true;
+      }
+    });
+    // TODO: implement initState
+    super.initState();
+  }
+
+  UserCredential? ress;
+  login() async {
+    res = await auth.signInWithEmailAndPassword(
+        email: "yahya@gmail.com", password: "123456789");
+
+    setState(() {});
+    ress = res;
+    print("This user ${ress!.user!.email}");
+  }
+
+  logout() {
+    auth.signOut();
+    ress = null;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Column(
       mainAxisAlignment: MainAxisAlignment.center,
-      children: const [Text("Hello")],
+      children: [
+        TextButton(
+          onPressed: () {
+            login();
+            // auth.signInWithCredential();
+          },
+          child: Text("LOGIN ${ress}"),
+        ),
+        TextButton(
+          onPressed: () {
+            logout();
+            // auth.signInWithCredential();
+          },
+          child: Text("LOGOUT "),
+        ),
+        Text("Hello ${auth.currentUser?.email} $isLogin"),
+      ],
     ));
   }
 }
