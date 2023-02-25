25f96ae1692ce84b4d140e07c66d1226c421c0fd
mac <zayns1914@gmail.com>
2023-02-25 08:39:57 +0200

    try badge

--- lib/main.dart
+++ lib/main.dart
@@ -75,6 +75,11 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          Badge.count(
+            count: 4,
+            isLabelVisible: true,
+            child: const Icon(Icons.notifications),
+          ),
           TextButton(
             onPressed: () {
               login();
